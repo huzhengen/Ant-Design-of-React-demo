c6041f0756019b062e614f47b8420fe91e985c63
hukai <huzhengen@163.com>
2020-07-26 18:59:38 +0800

    ant design demo

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Ant-Design-of-React-demo
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import FormDemo from './FormDemo'
+
+function App() {
+  return (
+    <div className="App">
+      <FormDemo/>
+    </div>
+  );
+}
+
+export default App;
